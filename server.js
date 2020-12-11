45f7a42f78dba37b7da78bab5dfd0f1736fc374d
clacla-s8 <clariceks8@gmail.com>
2020-12-11 17:54:31 -0300

    cofigurada porta

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const app = require("./src/app");
-const port = 8080;
+const porta = 8080;
 
-app.listen(process.env.PORT || port, () => {
-    console.log(`app está rodando na porta ${port}`);
-});
+app.listen(process.env.PORT || porta);
