b0143ca655f073bd70c6314d13a1e13768813f89
clacla-s8 <clariceks8@gmail.com>
2020-12-11 19:12:41 -0300

    corrigindo erros heroku

--- src/routes/sessaoRoute.js
+++ src/routes/sessaoRoute.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../controller/sessaoController")
-const sessaoClienteController = require('../controller/sessaoControllerCliente');
+
 
 router.post('/anunciante', controller.accessTokenAnunciante);
-router.post('/cliente', sessaoClienteController.accessTokenCliente);
+router.post('/cliente', controller.accessTokenCliente);
+
 
 module.exports = router;
