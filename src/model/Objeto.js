7b9c419e0165ef7b5b55828439c4a3a167e05e3e
clacla-s8 <clariceks8@gmail.com>
2021-02-14 18:16:49 -0300

    mudança no model objeto

--- src/model/Objeto.js
+++ src/model/Objeto.js
@@ -1,11 +1,10 @@
-const { Double } = require('bson')
 const { timeStamp } = require('console')
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const objetoSchema = new Schema({
     nome: { type: String, required: true },
-    preco: { type: Number, required: true },
+    preco: { type: String, required: true },
     img: { type: String, default: "", required: true },
     categoria: { type: String, required: true },
     isAlugado: { type: Boolean, required: true, default: false },
