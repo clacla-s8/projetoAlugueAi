555a5f395e206b701900670d2cf0b23dc565a3a2
clacla-s8 <clariceks8@gmail.com>
2021-02-21 21:59:08 -0300

    model

--- src/model/Objeto.js
+++ src/model/Objeto.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const objetoSchema = new Schema({
     nome: { type: String, required: true },
     preco: { type: String, required: true },
-    img: { type: String, default: "", required: true },
+    img: { type: String, required: true },
     categoria: { type: String, required: true },
     isAlugado: { type: Boolean, required: true, default: false },
     //usuarioId: { type: Schema.Types.ObjectId, ref: 'Usuario', required: true },
